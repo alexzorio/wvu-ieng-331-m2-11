523853a4af204153cd3b8f55eeba52fff0a664a2
Alex Zorio <apz00001@mix.wvu.edu>
2026-04-18 19:12:12 -0400

    Parameterized ABC.sql

--- sql/ABC.sql
+++ sql/ABC.sql
@@ -12,7 +12,7 @@ WITH ProductRevenue AS (
     SELECT
         product_id,
         SUM(price) as total_revenue
-    FROM olist.order_items
+    FROM order_items
     GROUP BY 1
 ),
 RunningTotals AS (
@@ -20,19 +20,19 @@ RunningTotals AS (
     SELECT
         product_id,
         total_revenue,
-        SUM(total_revenue) OVER (ORDER BY total_revenue DESC) as cumulative_revenue, -- Cumulative revenue for each product
-        SUM(total_revenue) OVER () as grand_total -- Grand total revenue for all products
+        SUM(total_revenue) OVER (ORDER BY total_revenue DESC) as cumulative_revenue,
+        SUM(total_revenue) OVER () as grand_total
     FROM ProductRevenue
 )
 -- Use when else statement to separate the different orders into their respective tiers by revenue percentage
 SELECT
     product_id,
     total_revenue,
-    (cumulative_revenue / grand_total) * 100 as pct_of_total, -- Percentage of total revenue for each product
+    (cumulative_revenue / grand_total) * 100 as pct_of_total,
     CASE
-        WHEN (cumulative_revenue / grand_total) <= 0.80 THEN 'A' -- Top 20% of revenue gets an A
-        WHEN (cumulative_revenue / grand_total) <= 0.95 THEN 'B' -- Next 15% gets a B
-        ELSE 'C' -- The rest gets a C
+        WHEN (cumulative_revenue / grand_total) <= $1 THEN 'A'
+        WHEN (cumulative_revenue / grand_total) <= $2 THEN 'B'
+        ELSE 'C'
     END as abc_tier
 FROM RunningTotals
-ORDER BY total_revenue DESC; -- Order by total revenue descending
+ORDER BY total_revenue DESC;
